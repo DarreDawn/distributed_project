28a958b8392416da8694efa4698708c82aad847e
DarreDawn <111307490+DarreDawn@users.noreply.github.com>
2024-04-14 18:24:01 +0300

    first

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar build/libs/gradle-wrapper.jar
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("java")
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+    id ("org.springframework.boot") version "2.4.5"
+    id ("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
 
 group = "org.example"
@@ -12,8 +15,28 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.springframework.boot:spring-boot-starter-web")
 }
 
-tasks.test {
+tasks.shadowJar {
+    archiveBaseName.set("your-app")
+    archiveVersion.set("0.1.0")
+    archiveClassifier.set("")
+}
+
+tasks {
+    register<Copy>("copyToLib") {
+        into(layout.buildDirectory.dir("libs"))
+        from(configurations.runtimeClasspath)
+    }
+}
+
+tasks.withType<Test> {
     useJUnitPlatform()
 }
+tasks.register("stage") {
+    dependsOn("build")
+}
+
+
+
